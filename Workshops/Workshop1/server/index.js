73ced45c4ad7483696d6fdff11b876652846efa5
Josué Soto <deivysoto66@gmail.com>
2021-05-24 22:06:12 -0600

    Workshop 1 advanced

--- Workshops/Workshop1/server/index.js
+++ Workshops/Workshop1/server/index.js
@@ -1,29 +1,32 @@
 const express = require('express');
-const cors = require("cors");
 const app = express();
 
+// database connection
+const mongoose = require("mongoose");
+const db = mongoose.connect("mongodb://127.0.0.1:27017/todo-api");
+// parser for the request body (required for the POST and PUT methods)
+const bodyParser = require("body-parser");
+const Task = require("./models/taskModel");
+
+app.use(bodyParser.json());
+
+const {
+  taskPatch,
+  taskPost,
+  taskGet,
+} = require("./controllers/taskController.js");
 
 // check for cors
+const cors = require("cors");
 app.use(cors({
-    domains: '*',
-    methods: "*"
+  domains: '*',
+  methods: "*"
 }));
 
+// listen to the task request
+app.get("/api/tasks", taskGet);
+app.post("/api/tasks", taskPost);
+app.patch("/api/tasks", taskPatch);
+app.put("/api/tasks", taskPatch);
 
-// listen to GET requests on /hello
-app.get('/hello', function (req, res) {
-    res.send('Hello World');
-});
-
-
-app.get('/tipocambio', function (req, res) {
-    res.send(`{
-    "TipoCompraDolares" : "608",
-    "TipoVentaDolares" : "621",
-    "TipoCompraEuros" : "731.85",
-    "TipoVentaEuros" : "761.9"
-  }`);
-});
-
-
-app.listen(8080, () => console.log(`Example app listening on port 8080!`))
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
